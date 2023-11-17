32daef27ab990e7bec7105871569ce514108e838
justice jennings <justice.l.jennings@gmail.com>
2023-11-17 13:15:59 -0500

    added authenticate and signupform component variables and exported.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import SignUpForm from './components/SignUpForm'
 import './App.css'
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <>
-
+    {SignUpForm};
     </>
   )
 }
---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,5 @@
+const SignUpForm = () => {
+return <h2>Sign Up</h2>
+};
+
+export default SignUpForm;
