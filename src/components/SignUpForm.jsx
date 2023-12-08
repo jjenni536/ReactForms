f09a94f490b4ab19ef1c491b967394cfb1a299e5
justice jennings <justice.l.jennings@gmail.com>
2023-12-07 20:33:35 -0500

    corrected function for signupform.

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,19 +1,37 @@
 import { useState } from "react";
+const SignUpForm = () => {
 const [username, setUsername] = useState("");
 const [password, setPassword] = useState("");
 const [error, setError] = useState(null);
 
-const SignUpForm = () => {
-return <h2>Sign Up</h2>;
-<form>
-    <label>
-        Username: <input />
-  </label>
-  <label>
-        Password: <input />
-  </label>
-    <button>Submit</button>
-</form>
-}
+const handleSubmit = (e) => {
+    e.preventDefault();
+};
+
+return (
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+    	<label>
+			Username: 
+			<input 
+				type="text" 
+				value={username} 
+				onChange={(e) => setUsername(e.target.value)} 
+          />
+        </label>
+        <label>
+				Password: 
+				<input 
+					type="password" 
+					value={password} 
+					onChange={(e) => setPassword(e.target.value)} 
+				/>
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default SignUpForm;
