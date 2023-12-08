83fbf3a1055248384958f59031d27d678f2d5ae8
justice jennings <justice.l.jennings@gmail.com>
2023-12-07 21:05:16 -0500

    fetched api and recieved token for signup.

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -4,13 +4,31 @@ const [username, setUsername] = useState("");
 const [password, setPassword] = useState("");
 const [error, setError] = useState(null);
 
-const handleSubmit = (e) => {
+const handleSubmit = async (e) => {
     e.preventDefault();
-};
+
+		try {const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup",
+		{ 
+			method: "POST", 
+			headers: { 
+				"Content-Type": "application/json" 
+			}, 
+			body: JSON.stringify({ 
+				username: username, 
+				password: password 
+			}) 
+		});
+const result = await response.json();
+console.log(result);
+
+    } catch (error) {
+      setError(error.message);
+    }
+}
 
 return (
     <div>
-      <h2>Sign Up</h2>
+      <h2>Sign Up</h2> {error && <p>{error}</p> }
       <form onSubmit={handleSubmit}>
     	<label>
 			Username: 
